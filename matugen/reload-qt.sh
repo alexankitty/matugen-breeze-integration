14a0f9b9132c92a87d305c3f0a6415e856cf09af
Alexandra <alexankitty@gmail.com>
2025-08-21 04:19:17 -0600

    fix sed mistakes

--- matugen/reload-qt.sh
+++ matugen/reload-qt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sed -i "s|color_scheme_path=/|color_scheme_path=//|" "$HOME/.config/qt6ct/qt6ct.conf"
-sed -i "s|color_scheme_path=/|color_scheme_path=//|" "$HOME/.config/qt5ct/qt5ct.conf"
+sed -Ei "s|color_scheme_path=/|color_scheme_path=//|" "$HOME/.config/qt6ct/qt6ct.conf"
+sed -Ei "s|color_scheme_path=/|color_scheme_path=//|" "$HOME/.config/qt5ct/qt5ct.conf"
 sleep 5
-sed -i "s|color_scheme_path=//|color_scheme_path=/|" "$HOME/.config/qt6ct/qt6ct.conf"
-sed -i "s|color_scheme_path=//|color_scheme_path=/|" "$HOME/.config/qt5ct/qt5ct.conf"
+sed -Ei "s|color_scheme_path=/+|color_scheme_path=/|" "$HOME/.config/qt6ct/qt6ct.conf"
+sed -Ei "s|color_scheme_path=/+|color_scheme_path=/|" "$HOME/.config/qt5ct/qt5ct.conf"
